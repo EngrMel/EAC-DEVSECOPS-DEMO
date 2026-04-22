72d05a02540a17f46b4eb3ae400a7ea8264f2423
EngrMel <romxzg@gmail.com>
2026-04-22 13:40:02 +0800

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello DevSecOps"
+
+@app.route("/profile")
+def profile():
+    name = request.args.get("name")
+    return f"<h1>Welcome {name}</h1>"
